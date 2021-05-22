9bc1e2e30e104e4860dc60d396fe6dae5f51a8e7
Ahmad Ramzy <ahmadramzy988@gmail.com>
2021-05-22 13:31:36 +0200

    added excel file uploading

--- src/AddStudent.js
+++ src/AddStudent.js
@@ -5,6 +5,10 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import AddIcon from "@material-ui/icons/Add";
 import axios from "axios";
 import { Alert } from "@material-ui/lab";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import { ExcelRenderer } from "react-excel-renderer";
+
 var success = false;
 
 export default class AddStudents extends Component {
@@ -30,6 +34,9 @@ export default class AddStudents extends Component {
     token: "",
     exam_id: "",
     error: "",
+    manually: 0,
+    upload: 0,
+    selectedFile: null,
   };
   constructor(props) {
     super(props);
@@ -113,6 +120,40 @@ export default class AddStudents extends Component {
           });
         });
     };
+    const enterManually = () => {
+      this.setState({
+        manually: 1,
+        upload: 0,
+      });
+    };
+    const uploadFile = () => {
+      this.setState({
+        upload: 1,
+        manually: 0,
+      });
+    };
+    const getData = () => {
+      let result = [];
+      ExcelRenderer(this.state.selectedFile, (err, resp) => {
+        if (err) {
+          console.log(err);
+        } else {
+          resp.rows.map((student) => result.push(student[0]));
+          this.setState({
+            allowedStudents: result,
+          });
+        }
+      });
+    };
+    const fileHandler = (event) => {
+      this.setState(
+        {
+          selectedFile: event.target.files[0],
+        },
+        getData
+      );
+    };
+
     return (
       <div>
         {success === true ? (
@@ -127,33 +168,75 @@ export default class AddStudents extends Component {
           <div></div>
         )}
         <br />
-        <TextField
-          id="outlined-multiline-static"
-          label="Add allowed Students"
-          multiline
-          rowsMax={this.state.allowedStudents.length}
-          variant="outlined"
-          onChange={handleAddStudents}
-          fullWidth
-          size="medium"
-          value={this.state.allowedStudents}
-        />
-        <br /> <br />
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          startIcon={
-            this.state.loading ? (
-              <CircularProgress size={20} color="secondary" />
-            ) : (
-              <AddIcon />
-            )
-          }
-          onClick={handleAllowedStudentsArray}
-        >
-          {this.state.loading ? "Adding students..." : "Add students"}
-        </Button>
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button onClick={enterManually}>Enter students manually</Button>
+          <Button onClick={uploadFile}>Upload an excel file</Button>
+        </ButtonGroup>
+        {this.state.manually === 1 ? (
+          <div>
+            <br />
+            <TextField
+              id="outlined-multiline-static"
+              label="Add allowed Students"
+              multiline
+              rowsMax={this.state.allowedStudents.length}
+              variant="outlined"
+              onChange={handleAddStudents}
+              fullWidth
+              size="medium"
+              value={this.state.allowedStudents}
+            />
+            <br /> <br />
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              startIcon={
+                this.state.loading ? (
+                  <CircularProgress size={20} color="secondary" />
+                ) : (
+                  <AddIcon />
+                )
+              }
+              onClick={handleAllowedStudentsArray}
+            >
+              {this.state.loading ? "Adding students..." : "Add students"}
+            </Button>{" "}
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {this.state.upload === 1 ? (
+          <div>
+            <br />{" "}
+            <input
+              id="contained-button-file"
+              type="file"
+              style={{ display: "none" }}
+              onChange={fileHandler.bind(this)}
+            />
+            <label htmlFor="contained-button-file">
+              <Button
+                variant="contained"
+                color="default"
+                component="span"
+                startIcon={<CloudUploadIcon />}
+              >
+                Choose an excel file
+              </Button>{" "}
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<AddIcon />}
+                onClick={handleSubmit}
+              >
+                Upload and add students
+              </Button>{" "}
+            </label>
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     );
   }
